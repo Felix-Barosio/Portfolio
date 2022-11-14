bf10c259fbd5aae1e94c33c85153faa529580bc3
Felix <barosiofelix@gmail.com>
2022-11-14 09:42:12 +0300

    Added import from personal

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,16 @@
 import React from "react";
 import Profile from "../../assets/images/profile3.png";
 import "./Header.css";
+import Personal from "./Personal";
 
 const Header = () => {
     return (
         <div id="home">
-            <div className="container header_container">
+            <div className="container header_cont">
                 <h4>Hello There I'm</h4>
                 <h1>Barosio Felix</h1>
                 <h4 className="text-light">Full-Stack Developer</h4>
+                <Personal />
                 <a href="#contact" className="scroll_down">
                     Scroll Down
                 </a>
