361b9574515d4d433cf6f320566095c37df741e7
Felix <barosiofelix@gmail.com>
2022-11-14 16:00:13 +0300

    Imported socials

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,6 +2,7 @@ import React from "react";
 import Profile from "../../assets/images/profile3.png";
 import "./Header.css";
 import Personal from "./Personal";
+import Socials from "./Socials";
 
 const Header = () => {
     return (
@@ -11,6 +12,7 @@ const Header = () => {
                 <h1>Barosio Felix</h1>
                 <h4 className="text-light">Full-Stack Developer</h4>
                 <Personal />
+                <Socials />
                 <a href="#contact" className="scroll_down">
                     Scroll Down
                 </a>
