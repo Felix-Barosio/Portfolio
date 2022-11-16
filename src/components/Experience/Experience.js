23745e7fdf22e72236779f302784c4a6fd27f6ee
Felix <barosiofelix@gmail.com>
2022-11-16 16:48:51 +0300

    Added skills cards

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -7,6 +7,24 @@ const Experience = () => {
         <section id='experience'>
             <h5>What Skills Do I Have</h5>
             <h2>My Experience</h2>
+
+            <div className='container expe_container'>
+
+                <div className='expe_frontend'>
+                    <h3>Frontend Development</h3>
+                    <div className='expe_content'>
+                        {renderExperience(frontEnd)}
+                    </div>
+                </div>
+
+                <div className='expe_backend'>
+                    <h3>Backend Development</h3>
+                    <div className='expe_content'>
+                        {renderExperience(backEnd)}
+                    </div>
+                </div>
+
+            </div>
         </section>
     )
 }
