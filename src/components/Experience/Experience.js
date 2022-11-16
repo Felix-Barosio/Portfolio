420028247fc1b8d8b2a16e67d7c4b607a4487663
Felix <barosiofelix@gmail.com>
2022-11-16 16:48:03 +0300

    Added experience file

---
+++ src/components/Experience/Experience.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+const Experience = () => {
+
+    return (
+        <section id='experience'>
+            <h5>What Skills Do I Have</h5>
+            <h2>My Experience</h2>
+        </section>
+    )
+}
+
+export default Experience
