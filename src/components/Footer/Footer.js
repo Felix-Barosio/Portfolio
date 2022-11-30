77689c2cfeb317fd2bb528888850c305ece00f55
Felix <barosiofelix@gmail.com>
2022-11-30 10:11:02 +0300

    Added footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React from "react"
 
-function Footer() {
+const Footer = () => {
     return (
-        <div>Footer</div>
+        <footer>
+            <a href="#home" className="footer_logo">BAROSIO</a>
+            <ul className="permalinks">
+                <li><a href="#home">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#experience">Experience</a></li>
+                {/* <li><a href="#services">Services</a></li> */}
+                <li><a href="#portfolio">Portfolio</a></li>
+                {/* <li><a href="#testimonials">Testimonials</a></li> */}
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+
+            <div className="footer_copyrights">
+                <small>&copy; 2022. Barosio Felix. All rights reserved</small>
+            </div>
+        </footer>
     )
 }
 
