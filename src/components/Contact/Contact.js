7c3b4e1acf980cccc9155cdd2cbe4f75ab2e1a5f
Felix <barosiofelix@gmail.com>
2022-12-02 10:25:55 +0300

    Added send email

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,11 +1,37 @@
 import React, { useRef } from 'react';
+import './Contact.css'
+// import { RiMessengerLine } from 'react-icons/ri'
+// import emailjs from '@emailjs/browser';
 
 const Contact = () => {
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        // emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY')
+        //     .then((result) => {
+        //         console.log(result.text);
+        //     }, (error) => {
+        //         console.log(error.text);
+        //     });
+
+        e.target.reset()
+    };
 
     return (
         <section id="contact">
             <h5>Get in Touch</h5>
             <h2>Contact Me</h2>
+            <div className='contact_container'>
+
+                <form ref={form} onSubmit={sendEmail}>
+                    <input type="text" name="name" placeholder="Your Full Name" required />
+                    <input type="email" name="email" placeholder="Your Email" required />
+                    <textarea name="message" rows="7" placeholder='Your Message' required />
+                    <button type="submit" className='btn btn-primary'>Send Message</button>
+                </form>
+            </div>
         </section>
     )
 }
