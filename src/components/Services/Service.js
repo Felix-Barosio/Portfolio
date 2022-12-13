0a3bbab02cac8008fa8d9547b81c5273def788d8
Felix <barosiofelix@gmail.com>
2022-12-13 15:18:02 +0300

    add: services

---
+++ src/components/Services/Service.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Service = () => {
+    return (
+        <section id='services'>
+            <h5>What I Offer</h5>
+            <h2>Services</h2>
+            <div className='container services_container'>
+            </div>
+        </section>
+    )
+}
+
+export default Service
