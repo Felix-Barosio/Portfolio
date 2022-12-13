63a5b676d2595f19b4fc644cabf251d435b1e8af
Felix <barosiofelix@gmail.com>
2022-12-13 18:58:17 +0300

    update: service offered

--- src/components/Services/Service.js
+++ src/components/Services/Service.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import './Service.css'
+import { RiCheckFill } from 'react-icons/ri'
 
 const Service = () => {
     const frontEndServices = ['HTML', 'CSS', 'Javascript (Vanilla)', 'React']
@@ -10,6 +12,15 @@ const Service = () => {
             <h5>What I Offer</h5>
             <h2>Services</h2>
             <div className='container services_container'>
+
+                <article className='service'>
+                    <div className='service_header'>
+                        <h3> Front End Development </h3>
+                    </div>
+                    <ul className='service_list'>
+                        {renderServices(frontEndServices)}
+                    </ul>
+                </article>
             </div>
         </section>
     )
