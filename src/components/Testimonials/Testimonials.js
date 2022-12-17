88546f96ba035ba141bd9a815e244c873551c3d5
Felix <barosiofelix@gmail.com>
2022-12-17 13:28:38 +0300

    add: cont

--- src/components/Testimonials/Testimonials.js
+++ src/components/Testimonials/Testimonials.js
@@ -1,4 +1,10 @@
 import React from 'react'
+import { Swiper } from 'swiper/react';
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 
 const Testimonial = () => {
 
@@ -6,6 +12,17 @@ const Testimonial = () => {
         <section id='testimonials'>
             <h5>Client Reviews</h5>
             <h2>Testimonials</h2>
+            <div className="container testimonial_container">
+                <Swiper
+                    modules={[Navigation, Pagination, Scrollbar, A11y]}
+                    spaceBetween={40}
+                    slidesPerView={1}
+                    navigation
+                    pagination={{ clickable: true }}
+                    scrollbar={{ draggable: true }}
+                >
+                </Swiper>
+            </div>
         </section>
     )
 }
