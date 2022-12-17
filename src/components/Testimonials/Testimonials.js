92cb3975dcfc10e43d233e68079768da81b552bc
Felix <barosiofelix@gmail.com>
2022-12-17 13:30:09 +0300

    add: reveiws data

--- src/components/Testimonials/Testimonials.js
+++ src/components/Testimonials/Testimonials.js
@@ -7,6 +7,38 @@ import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
 
 const Testimonial = () => {
+    const reviews = [
+        {
+            id: 1,
+            avatar: Pic,
+            name: 'Beckett',
+            review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus pariatur nemo nisi deserunt accusantium harum fugit sapiente vel, amet ipsum veniam, animi quisquam tempora officiis itaque esse, necessitatibus sequi. Commodi."
+        },
+        {
+            id: 2,
+            avatar: Pic,
+            name: 'Rick Castle',
+            review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus pariatur nemo nisi deserunt accusantium harum fugit sapiente vel, amet ipsum veniam, animi quisquam tempora officiis itaque esse, necessitatibus sequi. Commodi."
+        },
+        {
+            id: 3,
+            avatar: Pic,
+            name: 'Jessica',
+            review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus pariatur nemo nisi deserunt accusantium harum fugit sapiente vel, amet ipsum veniam, animi quisquam tempora officiis itaque esse, necessitatibus sequi. Commodi."
+        },
+        {
+            id: 4,
+            avatar: Pic,
+            name: 'Mike',
+            review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus pariatur nemo nisi deserunt accusantium harum fugit sapiente vel, amet ipsum veniam, animi quisquam tempora officiis itaque esse, necessitatibus sequi. Commodi."
+        },
+        {
+            id: 5,
+            avatar: Pic,
+            name: 'Sam',
+            review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus pariatur nemo nisi deserunt accusantium harum fugit sapiente vel, amet ipsum veniam, animi quisquam tempora officiis itaque esse, necessitatibus sequi. Commodi."
+        }
+    ]
 
     return (
         <section id='testimonials'>
